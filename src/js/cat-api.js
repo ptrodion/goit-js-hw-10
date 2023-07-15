f70c6885b38fb1f3aea86b3acb907ab76b94e783
ptrodion <rdnpetrenko@gmail.com>
2023-07-15 23:25:27 +0100

    4

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -4,16 +4,16 @@ axios.defaults.headers.common['x-api-key'] =
   'live_cK5eVLnSrGBmmngczTMu0cNbQDslW9YnXLOvdntREOq3waRUZWSzZjuJ2UgHlfne';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
-const BASE_URL = 'https://api.thecatapi.com';
+// const BASE_URL = 'https://api.thecatapi.com/';
 
 const customConfig = {
-  baseURL: BASE_URL,
+  baseURL: 'https://api.thecatapi.com/',
 };
 
 const instance = axios.create(customConfig);
 
 export function fetchBreeds() {
-  return instance.get('/v1/breeds').then(resp => {
+  return instance.get('v1/breeds').then(resp => {
     if (resp.status !== 200) {
       throw new Error(resp.statusText);
     }
@@ -23,7 +23,7 @@ export function fetchBreeds() {
 }
 
 export function fetchCatByBreed(idCat) {
-  return instance.get(`/v1/images/search?breed_ids=${idCat}`).then(resp => {
+  return instance.get(`v1/images/search?breed_ids=${idCat}`).then(resp => {
     if (resp.status !== 200) {
       throw new Error(resp.statusText);
     }
